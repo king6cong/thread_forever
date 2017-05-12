97db814c075f75b4a63b905356b1368d38a62218
king6cong <king6cong@gmail.com>
2017-05-12 20:54:52 +0800

    fix race conditions

--- src/thread_handle.rs
+++ src/thread_handle.rs
@@ -1,5 +1,5 @@
 use std::sync::{Arc, Mutex, Condvar};
-use std::time::Duration;
+// use std::time::Duration;
 
 #[derive(Debug, PartialEq)]
 enum ThreadStatus {
@@ -22,43 +22,54 @@ impl ThreadHandle {
     pub fn wait_for_thread_up(&self) {
         let (ref lock, ref cvar) = *self.status.clone();
         let mut status = lock.lock().unwrap();
-        debug!("wait_for_thread_up: enter");
-        loop {
-            let result = cvar.wait_timeout(status, Duration::from_millis(10))
-                .unwrap();
-            debug!("10 milliseconds have passed: result: {:?} {:?}",
-                   result,
-                   *result.0);
-            status = result.0;
-            if let ThreadStatus::Up = *status {
-                trace!("wait_for_thread_up: exit");
-                break;
-            }
+        trace!("wait_for_thread_up: enter");
+        // loop {
+        //     let result = cvar.wait_timeout(status, Duration::from_millis(10))
+        //         .unwrap();
+        //     debug!("10 milliseconds have passed: result: {:?} {:?}",
+        //            result,
+        //            *result.0);
+        //     status = result.0;
+        //     if let ThreadStatus::Up = *status {
+        //         trace!("wait_for_thread_up: exit");
+        //         break;
+        //     }
+        // }
+        while *status != ThreadStatus::Up {
+            status = cvar.wait(status).unwrap();
+            trace!("waked up: {:?}", *status);
         }
+        trace!("wait_for_thread_up: exit");
     }
 
     pub fn notify_thread_up(&self) {
         let (ref lock, ref cvar) = *self.status;
         let mut status = lock.lock().unwrap();
         *status = ThreadStatus::Up;
-        debug!("notify the condvar that thread is up.");
-        cvar.notify_one();
+        trace!("notify the condvar that thread is up.");
+        cvar.notify_all();
     }
 
     /// return false if init is already done
-    pub fn thread_init(&self) -> bool {
-        let (ref lock, _) = *self.status;
+    pub fn thread_need_init(&self) -> bool {
+        let (ref lock, ref cvar) = *self.status;
         let mut status = lock.lock().unwrap();
+        trace!("thread_need_init status: {:?}", *status);
         match *status {
             ThreadStatus::Uninitialized => {
                 *status = ThreadStatus::Pending;
                 true
             }
             ThreadStatus::Pending => {
-                self.wait_for_thread_up();
+                trace!("pending wait 0");
+                while *status != ThreadStatus::Up {
+                    status = cvar.wait(status).unwrap();
+                    trace!("pending waked up: {:?}", *status);
+                }
+                trace!("pending wait 1");
                 false
             }
-            _ => false,
+            ThreadStatus::Up => false,
         }
     }
 }
--- src/thread_worker.rs
+++ src/thread_worker.rs
@@ -1,51 +1,59 @@
 use std::thread;
+// use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard, Arc};
 use thread_handle::ThreadHandle;
 pub use errors::*;
 use Payload;
 
 pub struct ThreadWorker<T> {
+    // payload: Arc<RwLock<T>>,
     pub payload: T,
     handle: ThreadHandle,
+    name: String,
 }
 
 impl<T> ThreadWorker<T>
-    where T: Payload + Clone + Send + 'static
+    where T: Payload + Clone + Send + Sync + 'static
 {
     pub fn new(payload: T) -> Self {
+        let name = payload.name();
         ThreadWorker {
+            // payload: Arc::new(RwLock::new(payload)),
             payload: payload,
             handle: ThreadHandle::new(),
+            name: name,
         }
     }
 
     pub fn spin_up(&self) {
         let handle = self.handle.clone();
         let payload = self.payload.clone();
-        let name = self.payload.name();
-
-        if !self.handle.thread_init() {
-            info!("{} worker is already initialized and we return directly!",
-                  name);
+        let name = self.name.clone();
+        trace!("0");
+        if !self.handle.thread_need_init() {
+            info!("{} worker already initialized, return directly!", name);
             return;
         }
+        trace!("1");
 
         let _ = thread::Builder::new()
             .name(format!("t:{}_watchdog", name))
             .spawn(move || -> Result<()> {
-                debug!("{}_watchdog started!", name);
+                trace!("{}_watchdog spawn", name);
                 while {
                           let handle = handle.clone();
                           let payload = payload.clone();
                           thread::Builder::new()
                               .name(format!("t:{}", name))
                               .spawn(move || -> Result<()> {
+                        trace!("2");
+                        handle.notify_thread_up();
+                        trace!("3");
 
-                                         handle.notify_thread_up();
+                        let _ = payload.thread_func();
 
-                                         let _ = payload.thread_func();
 
-                                         Ok(())
-                                     })?
+                        Ok(())
+                    })?
                               .join()
                               .is_err()
                       } {
@@ -53,55 +61,76 @@ impl<T> ThreadWorker<T>
                 }
                 Ok(())
             });
-
+        trace!("4");
         self.handle.wait_for_thread_up();
+        trace!("5");
     }
 }
 
 #[cfg(test)]
 mod tests {
+    extern crate env_logger;
     #[allow(unused_imports)]
     use super::*;
     use std::time::Duration;
 
-    #[test]
+    lazy_static! {
+        pub static ref WORKER: ThreadWorker<Test> = {
+            let payload = Test::new();
+            let worker = ThreadWorker::new(payload);
+            worker
+        };
+    }
+
+    #[derive(Clone)]
+    pub struct Test {}
+
+    impl Payload for Test {
+        type Result = Result<()>;
+
+        fn name(&self) -> String {
+            "thread_forever_test".to_string()
+        }
+
+        fn thread_func(&self) -> Result<()> {
+            loop {
+                thread::sleep(Duration::from_millis(200));
+                info!("one loop iter");
+            }
+        }
+    }
+
+    impl Test {
+        fn new() -> Self {
+            Test {}
+        }
+        fn test_read(&self) {
+            info!("test_read");
+        }
+    }
+
     fn test_thread_forever() {
-
-        lazy_static! {
-            pub static ref WORKER: ThreadWorker<Test> = {
-                let payload = Test::new();
-                let worker = ThreadWorker::new(payload);
-                worker
-            };
-        }
-
-        #[derive(Clone)]
-        pub struct Test {}
-
-        impl Payload for Test {
-            type Result = Result<()>;
-
-            fn name(&self) -> String {
-                "thread_forever_test".to_string()
-            }
-
-            fn thread_func(&self) -> Result<()> {
-                loop {
-                    thread::sleep(Duration::from_millis(200));
-                    println!("one loop iter");
-                }
-            }
-        }
-
-        impl Test {
-            fn new() -> Self {
-                Test {}
-            }
-        }
-
+        let _ = env_logger::init();
         WORKER.spin_up();
         WORKER.spin_up();
         WORKER.spin_up();
+        WORKER.spin_up();
+        WORKER.payload.test_read();
         thread::sleep(Duration::from_millis(600));
+    }
+
+    #[test]
+    fn test_1() {
+        test_thread_forever();
+    }
+
+    #[test]
+    fn test_2() {
+        test_thread_forever();
+    }
+
+    #[test]
+    fn test_3() {
+        test_thread_forever();
     }
 }
